90516a755d1c766a1a305439ec633aee8aefc488
MondoBiscuit <bradleofabian@gmail.com>
2018-06-19 13:26:05 -0400

    giphy api search works

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
 
+    var userInput = "";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=Mqi5t09DvIKIDGnmszL6uvWuCml9QX8o&limit=10";
+    
+    //clickfunction for the userInput///
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+        //obtain a refference to the div where the gifs go//
+
+        //create and save a div that will be a gif//
+
+        //append the gif to the div//
+
+
+      });
+
+        
+
+    
 
 });
+
