3399a5284bac57bbe8a7490595167a8bf9019a27
MondoBiscuit <bradleofabian@gmail.com>
2018-06-19 13:58:29 -0400

    wrote out function to creat gif/rating when user submits

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 $(document).ready(function() {
+    $("#addCelebrity").on("click", function() {
+    var userInput = $(this).attr("");
 
-    var userInput = "";
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=Mqi5t09DvIKIDGnmszL6uvWuCml9QX8o&limit=10";
     
-    //clickfunction for the userInput///
+    //ajax to call the api//
     
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
         console.log(response)
-        //obtain a refference to the div where the gifs go//
+       
+        var results = response.data;
 
-        //create and save a div that will be a gif//
+          // Looping over every result item
+          for (var i = 0; i < results.length; i++) {
 
-        //append the gif to the div//
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#celebrities").prepend(gifDiv);
+            }
+
+        }
+
+        
+
+        
 
 
       });
@@ -22,6 +55,6 @@ $(document).ready(function() {
         
 
     
-
+    });    
 });
 
