deef34ec2b4f41ec5aa7910dd2fc5e9721661904
MondoBiscuit <bradleofabian@gmail.com>
2018-06-21 10:11:24 -0400

    fixed gifs, they now respond to clicks

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 $(document).ready(function() {
 
- 
-
-
-
     $("button").on("click", function() {
       console.log("sumbit pressed");
       
      var person = $(this).attr("data-person");
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=Mqi5t09DvIKIDGnmszL6uvWuCml9QX8o&limit=10";
-
-    
-
-
     //ajax to call the api//
     
     $.ajax({
@@ -24,25 +16,31 @@ $(document).ready(function() {
         var results = response.data;
         console.log(results)
           // Looping over every result item
-          for (var  j = 0; j < results.length; j++) {
+          for (var  i = 0; i < results.length; i++) {
 
           // Only taking action if the photo has an appropriate rating
-            if (results[j].rating !== "r" && results[j].rating !== "pg-13") {
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
               // Creating a div with the class "item"
-              var gifDiv = $("<div class='item'>");
+              var gifDiv = $("<div>");
 
               // Storing the result item's rating
-              var rating = results[j].rating;
+              var rating = results[i].rating;
 
               // Creating a paragraph tag with the result item's rating
               var p = $("<p>").text("Rating: " + rating);
 
               // Creating an image tag
-              var personImage = $("<img data-state='still' class='gif'>");
-
+              var personImage = $("<img>");
+              personImage.addClass('perImg')
               // Giving the image tag an src attribute of a proprty pulled off the
               // result item
-              personImage.attr("src", results[j].images.fixed_height.url);
+              personImage.attr("src", results[i].images.fixed_height_still.url);
+
+              personImage.attr('data-still',results[i].images.fixed_height_still.url);
+
+              personImage.attr('data-animate', results[i].images.fixed_height.url);
+
+              personImage.attr('data-state', 'still');
 
               // Appending the paragraph and personImage we created to the "gifDiv" div we created
               gifDiv.append(p);
@@ -53,28 +51,34 @@ $(document).ready(function() {
 
             }
           }
+          $('.perImg').on('click', function() {
+            
+            var state = $(this).attr('data-state'); 
+            console.log(this);
+
+            if (state == 'still') {
+            
+            $(this).attr('src', $(this).data('animate'));
+            
+            $(this).attr('data-state', 'animate');
+
+            } else {
+                    
+            $(this).attr('src', $(this).data('still'));
+            
+            $(this).attr('data-state', 'still');
+            }      
+        });
        });
 
-     }); 
+     });
      
 
-     $(".gif").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
+    
     });
 
     //user submitfunction//
-   /* function submitButtonClicked() {
+    /*function submitButtonClicked() {
       var userInput = $('#data-celebrity').val();
   
       if (userInput) {
@@ -82,5 +86,5 @@ $(document).ready(function() {
       }
   } submitButtonClicked()*/
      
-});
+
 
