3cab1305133509b10d39bc32362787f98f7b4d0c
MondoBiscuit <bradleofabian@gmail.com>
2018-06-21 11:26:43 -0400

    no errors in code yet generated gifs not responding

--- app.js
+++ app.js
@@ -75,14 +75,90 @@ $(document).ready(function() {
           //capture input and submit to have gifs and buttons appear//
 
            //user submitfunction//
-          var inputGif = '';
+          var inputGif = [''];
           console.log(inputGif)
 
-    
-     
+          // function to add button//
+          $('#data-celebrity').on('click', function() {
+            var celebrityGif = $("#celebrityInput").val();
+            var newButton = $("<button/>").addClass( "button").attr('data-person',celebrityGif).html(celebrityGif).css({'margin': '5px'});
 
+            $("#celebrityButtons").append(newButton);
+                console.log("Work");
+
+                var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + celebrityGif + "&api_key=Mqi5t09DvIKIDGnmszL6uvWuCml9QX8o&limit=10";
+
+                console.log(queryURL);
+         
+         // call api//
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+          
+           
+            var results = response.data;
+            console.log(results)
+              // Looping over every result item
+              for (var  i = 0; i < results.length; i++) {
     
-    });
+              // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                  // Creating a div with the class "item"
+                  var gifDiv = $("<div>");
+    
+                  // Storing the result item's rating
+                  var rating = results[i].rating;
+    
+                  // Creating a paragraph tag with the result item's rating
+                  var p = $("<p>").text("Rating: " + rating);
+    
+                  // Creating an image tag
+                  var personImage = $("<img>");
+                  personImage.addClass('perImg')
+                  // Giving the image tag an src attribute of a proprty pulled off the
+                  // result item
+                  personImage.attr("src", results[i].images.fixed_height_still.url);
+    
+                  personImage.attr('data-still',results[i].images.fixed_height_still.url);
+    
+                  personImage.attr('data-animate', results[i].images.fixed_height.url);
+    
+                  personImage.attr('data-state', 'still');
+    
+                  // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                  gifDiv.append(p);
+                  gifDiv.append(personImage);
+    
+                  // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                  $("#celebrities").prepend(gifDiv);
+    
+                }
+              }
+              $('.perImg').on('click', function() {
+                
+                var state = $(this).attr('data-state'); 
+                console.log(this);
+    
+                if (state == 'still') {
+                
+                $(this).attr('src', $(this).data('animate'));
+                
+                $(this).attr('data-state', 'animate');
+    
+                } else {
+                        
+                $(this).attr('src', $(this).data('still'));
+                
+                $(this).attr('data-state', 'still');
+                }      
+            });
+           });
+          });
+
+          $("#celebrityInput").val("");
+          return false;
+    })
 
    
 
